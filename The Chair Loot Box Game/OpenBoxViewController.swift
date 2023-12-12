9d033d056bb5f3e35ac81b84862fe7ad46707c0c
dsarell1 <sarell.dylan20@gmail.com>
2023-12-12 18:01:03 -0500

    Open Loot Boxes Works and your Collection depends on what you have opened!! ;)

---
+++ The Chair Loot Box Game/OpenBoxViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OpenBoxViewController.swift
+//  The Chair Loot Box Game
+//
+//  Created by Guest User on 12/12/23.
+//
+
+import UIKit
+
+class OpenBoxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
